56de7479c861ea56988df1b179b933980c6840d9
KimAReum1 <117414706+KimAReum1@users.noreply.github.com>
2022-11-18 12:05:43 +0900

    Add files via upload

--- js/m_main.js
+++ js/m_main.js
@@ -1,22 +1,30 @@
-// $(document).ready(function () {
-//     var scene01Num = 1;
+$(document).ready(function () {
 
-//     $(window).scroll(function () {
-//         var scrollTop = $(document).scrollTop();
-//         if (scrollTop > 10 && scene01Num == 1) {
-//             scene01Num = 0;
-//             $('#skill_m .in_box01').stop().delay(0).css({ top: '100px' }).animate({ top: '20px', opacity: '1' }, 1000);
-//             $('#skill_m .in_box02').stop().delay(500).css({ top: '100px' }).animate({ top: '30px', opacity: '1' }, 1000);
-//             $('#skill_m .in_box03').stop().delay(1000).css({ top: '100px' }).animate({ top: '70px', opacity: '1' }, 1000);
-//             $('#skill_m .in_box04').stop().delay(1500).css({ top: '100px' }).animate({ top: '60px', opacity: '1' }, 1000);
-//         }
-//         else if (scrollTop < 5 && scene01Num == 0) {
-//             scene01Num = 1;
-//             $('#skill_m .in_box01').stop().css({ top: '20px', opacity: '1' }).animate({ top: '100px' }, 1000);
-//             $('#skill_m .in_box02').stop().css({ top: '30px', opacity: '1' }).animate({ top: '100px' }, 1000);
-//             $('#skill_m .in_box03').stop().css({ top: '70px', opacity: '1' }).animate({ top: '100px' }, 1000);
-//             $('#skill_m .in_box04').stop().css({ top: '60px', opacity: '1' }).animate({ top: '100px' }, 1000);
 
-//         }
-//     });
-//});
+    const menu = $('#header #nav li');
+    const menu_x = $('.m_fixnav .m_fix_mn li  ');
+    const contents = $('#wrap .mobile');
+
+
+
+    menu.click(function (e) {
+        e.preventDefault();
+        let i = $(this).index();
+        let section = $(contents).eq(i);
+
+        let tg = section.offset().top;
+
+        $('html, body').stop().animate({ scrollTop: tg });
+    });
+
+    menu_x.click(function (e) {
+        e.preventDefault();
+        i = $(this).index();
+        section = $(contents).eq(i);
+
+        tg = section.offset().top;
+
+        $('html, body').stop().animate({ scrollTop: tg });
+    });
+
+});
--- js/main.js
+++ js/main.js
@@ -11,6 +11,33 @@ $(document).ready(function () {
     }
 
 
+    const menu = $('#header #nav li');
+    const menu_x = $('.fixnav .fix_mn li  ');
+    const contents = $('#wrap > section');
+
+
+
+    menu.click(function (e) {
+        e.preventDefault();
+        let i = $(this).index();
+        let section = $(contents).eq(i);
+
+        let tg = section.offset().top;
+
+        $('html, body').stop().animate({ scrollTop: tg });
+    });
+
+    menu_x.click(function (e) {
+        e.preventDefault();
+        i = $(this).index();
+        section = $(contents).eq(i);
+
+        tg = section.offset().top;
+
+        $('html, body').stop().animate({ scrollTop: tg });
+    });
+
+
 
 
 
